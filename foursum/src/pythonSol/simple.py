8d2058e8599a089e3fb881f069b309c6b6b3279b
HogniJacobsen <42565152+HogniJacobsen@users.noreply.github.com>
2019-03-01 16:57:16 +0100

    Working :)

--- foursum/src/pythonSol/simple.py
+++ foursum/src/pythonSol/simple.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import sys
 from algs4.sorting import merge
-#from algs4.searching import binary_search_st
 
 N = int(sys.stdin.readline())
 vals = list(map(int, sys.stdin.readlines()))
@@ -18,26 +17,36 @@ vals = list(map(int, sys.stdin.readlines()))
 
 merge.sort(vals)
 
-def binary_search_recursive(needle, haystack, index = 0):
-    global midway
-    midway = (len(haystack)) // 2
-    if midway < 2:
-        return False
-    if haystack[midway] == needle:
-        return True
-    else:
-        if needle > haystack[midway]:
-            return binary_search_recursive(needle, haystack[midway:], index + midway)            
-        else:
-            return binary_search_recursive(needle, haystack[:midway], index)
+# def binary_search_recursive(needle, haystack, index = 0):
+#     global midway
+#     midway = (len(haystack)) // 2
 
+#     if midway == 0 or len(haystack) == 0:
+#         return False
+
+#     if haystack[midway] == needle:
+#         return True
+#     else:
+#         if needle > haystack[midway]:
+#             return binary_search_recursive(needle, haystack[midway:], index + midway)            
+#         else:
+#             return binary_search_recursive(needle, haystack[:midway], index)
+
+       
 for i in range(0, N):
     for j in range(i+1, N):
         for k in range(j+1, N):
-            needle = int(vals[i] + vals[j] + vals[k])
-            if binary_search_recursive((-1 * needle), vals) == True:
-                print(i,j,k,midway,file=sys.stderr)
-                print(True)
-                sys.exit()
+            low = k + 1
+            high = N - 1
+            while low <= high:
+                middle = (low + high) // 2
+                tot = vals[middle] + vals[i] + vals[j] + vals[k]
+                if tot == 0:
+                    print(i,j,k,middle,file=sys.stderr)
+                    print(True)
+                    sys.exit()
+                if tot > 0: 
+                    high = middle - 1
+                if tot < 0:
+                    low = middle + 1
 print(False)
-            
